0026da1d2b477ad6ec0f0d2d97bd8c200f788ea7
spooke <spooke@spooke.spooke-domain>
2023-10-25 19:30:25 +0200

    Release 1.0 SpookeGen

--- GUI.py
+++ GUI.py
@@ -3,12 +3,13 @@ import random
 import string
 import pyperclip
 
-# Fonction pour générer un mot de passe
+# Fonction pour générer un mot de passe en fonction des paramètres de l'utilisateur
 def generate_password():
-    # Récupérez les critères saisis par l'utilisateur
+    # Récuperer les critères saisis par l'utilisateur
     length = int(length_entry.get())
     use_uppercase = uppercase_var.get()
     use_digits = digits_var.get()
+    use_special_chars = special_chars_var.get()
 
     # Liste de caractères possibles pour le mot de passe
     characters = string.ascii_lowercase
@@ -16,36 +17,38 @@ def generate_password():
         characters += string.ascii_uppercase
     if use_digits:
         characters += string.digits
+    if use_special_chars:
+        characters += string.punctuation
 
-    # Générez un mot de passe aléatoire avec la longueur spécifiée
+    # Générer un mot de passe aléatoire avec la longueur spécifiée
     password = ''.join(random.choice(characters) for _ in range(length))
     
-    # Affichez le mot de passe généré
+    # Afficher le mot de passe généré
     password_label.config(text="Mot de passe généré : " + password)
 
-    # Copiez le mot de passe dans le presse-papiers
+    # Copier le mot de passe dans le presse-papiers
     pyperclip.copy(password)
 
-# Créez une instance de la classe Tk
+# Creer une instance de la classe Tk
 app = tk.Tk()
 
-# Définissez le titre de la fenêtre principale
+# Définir le titre de la fenêtre principale
 app.title("Générateur de Mot de Passe")
 
-# Définissez la taille initiale de la fenêtre
+# Définir la taille initiale de la fenêtre
 app.geometry("400x250")
 
-# Ajoutez un libellé pour expliquer le but du programme
+# Ajouter un libellé pour expliquer le but du programme
 label = tk.Label(app, text="Bienvenue dans le Générateur de Mot de Passe")
 label.pack()
 
-# Ajoutez un champ de texte pour la longueur du mot de passe
-length_label = tk.Label(app, text="Longueur du mot de passe :")
+# Ajouter un champ de texte pour la longueur du mot de passe
+length_label = tk.Label(app, text="Nombre de caractères souhaité :")
 length_label.pack()
 length_entry = tk.Entry(app)
 length_entry.pack()
 
-# Ajoutez des cases à cocher pour les critères du mot de passe
+# Ajouter des cases à cocher pour les critères du mot de passe
 uppercase_var = tk.IntVar()
 uppercase_check = tk.Checkbutton(app, text="Inclure des lettres majuscules", variable=uppercase_var)
 uppercase_check.pack()
@@ -54,15 +57,20 @@ digits_var = tk.IntVar()
 digits_check = tk.Checkbutton(app, text="Inclure des chiffres", variable=digits_var)
 digits_check.pack()
 
-# Ajoutez un bouton pour générer le mot de passe
+# Ajouter des cases à cocher pour les caractères spéciaux
+special_chars_var = tk.IntVar()
+special_chars_check = tk.Checkbutton(app, text="Inclure des caractères spéciaux", variable=special_chars_var)
+special_chars_check.pack()
+
+# Ajouter un bouton pour générer le mot de passe
 generate_button = tk.Button(app, text="Générer le mot de passe", command=generate_password)
 generate_button.pack()
 
-# Ajoutez un bouton pour copier le mot de passe dans le presse-papiers
+# Ajouter un bouton pour copier le mot de passe dans le presse-papiers
 copy_button = tk.Button(app, text="Copier dans le presse-papiers", command=lambda: pyperclip.copy(password_label.cget("text").split(" : ")[1]))
 copy_button.pack()
 
-# Ajoutez un libellé pour afficher le mot de passe généré
+# Ajouter un libellé pour afficher le mot de passe généré
 password_label = tk.Label(app, text="")
 password_label.pack()
 
--- README.md
+++ README.md
@@ -14,25 +14,25 @@ Ajouter des boutons pour générer un mot de passe et pour copier le mot de pass
 
 
 
-Étape 2 : Création d'une fonction de génération de mot de passe
+[Étape 2 : Création d'une fonction de génération de mot de passe [Done]
 
 Écrire une fonction qui génère un mot de passe aléatoire en utilisant des caractères alphanumériques et spéciaux.
 La fonction devrait prendre en compte la longueur souhaitée du mot de passe et les types de caractères que l'utilisateur souhaite inclure (lettres majuscules, lettres minuscules, chiffres, caractères spéciaux).
-Générer un mot de passe aléatoire basé sur les paramètres de l'utilisateur.
+Générer un mot de passe aléatoire basé sur les paramètres de l'utilisateur.]
 
 
-Étape 3 : Gestion des événements de l'interface utilisateur
+[Étape 3 : Gestion des événements de l'interface utilisateur [DONE]
 
 Définir des gestionnaires d'événements pour les boutons de génération de mot de passe et de copie dans le presse-papiers.
 Lorsque l'utilisateur clique sur le bouton de génération de mot de passe, appelez la fonction de génération de mot de passe et affichez le résultat dans un champ de texte.
-Lorsque l'utilisateur clique sur le bouton de copie dans le presse-papiers, copiez le mot de passe généré dans le presse-papiers de l'utilisateur.
+Lorsque l'utilisateur clique sur le bouton de copie dans le presse-papiers, copiez le mot de passe généré dans le presse-papiers de l'utilisateur.]
 
 
 
-Étape 4 : Exécution de l'application
+[Étape 4 : Exécution de l'application [DONE]
 
 Exécuter la boucle principale de tkinter pour afficher l'interface utilisateur.
-Attendre que l'utilisateur interagisse avec l'application.
+Attendre que l'utilisateur interagisse avec l'application.]
 
 
 
